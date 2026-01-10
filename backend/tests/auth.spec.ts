ee40c9f1fb64a5baf607969f35e5ae232acd8503
Anderson Luiz Jacobsen <167909746+andersonjacobsen@users.noreply.github.com>
2026-01-10 14:07:18 -0300

    test: finalize appointments flow and add auth test (TDD)

---
+++ backend/tests/auth.spec.ts
@@ -0,0 +1,36 @@
+import request from "supertest";
+import { describe, it, expect, beforeAll } from "vitest";
+import { app } from "../src/app";
+import { prisma } from "../src/lib/prisma";
+import bcrypt from "bcryptjs";
+
+describe("Authentication", () => {
+  beforeAll(async () => {
+    await prisma.user.deleteMany();
+
+    const passwordHash = await bcrypt.hash("123456", 8);
+
+    await prisma.user.create({
+      data: {
+        name: "Anderson",
+        email: "anderson@email.com",
+        password: passwordHash,
+      },
+    });
+  });
+
+  it("should be able to authenticate with valid credentials", async () => {
+    const response = await request(app).post("/v1/auth/login").send({
+      email: "anderson@email.com",
+      password: "123456",
+    });
+
+    expect(response.status).toBe(200);
+
+    expect(response.body).toHaveProperty("token");
+    expect(response.body.user).toMatchObject({
+      name: "Anderson",
+      email: "anderson@email.com",
+    });
+  });
+});
